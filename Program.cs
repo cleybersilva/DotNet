4c21c8715954284f55e48dfd345c57ff13f0580d
cleybersilva <cleyber@foconapratica.com>
2021-07-04 16:26:46 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int numeroDeVezes = 10;
+          for (int i=0; i < numeroDeVezes; i++)
+          {
+              Console.WriteLine($"Bem-vindos ao Curso de .Net {i}");
+          }
+        }
+    }
+}
